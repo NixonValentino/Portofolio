d7319efba6303cad637b20a8a0b47062bef5b586
Nixon <Nicocoyo02@gmail.com>
2025-08-19 14:27:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".auto-type",
+    {
+        strings : ['連絡先' , 'Selamat Datang', 'こんにちは', '連絡先' , 'Selamat Datang', 'こんにちは', 'Contact Info'],
+        typeSpeed : 150,
+        backSpeed : 150,
+        looped : true
+    }
+)
+
+var typed = new Typed(".typing",
+    {
+        strings : ['Welcome !!!' , 'To' , 'My' , 'Portofolio'],
+        typeSpeed : 150,
+        backSpeed : 150,
+        looped : true
+    }
+)
+
